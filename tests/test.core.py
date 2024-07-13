d686626e5880265a1dd0e17527398cf22e4ac0ff
Henrique Marques Santos Silva <henriquemarquessantossilva@hotmail.com>
2024-07-12 23:26:38 -0300

    up_12_7_2024

--- tests/test.core.py
+++ tests/test.core.py
@@ -1,4 +1,26 @@
 import igraph as ig
+import matplotlib.pyplot as plt
 
-g = ig.Graph([(0,1),(0,2),(0,3),(2,3),(2,4)])
-ig.plot(g)
+g = ig.Graph(6, [(3, 2), (3, 4), (2, 1), (4,1), (4, 5), (1, 0), (5, 0)],
+    directed=True)
+
+g.es["capacity"] = [7, 8, 1, 2, 3, 4, 5]
+
+flow = g.maxflow(3, 0, capacity=g.es["capacity"])
+
+print("Max flow:", flow.value)
+print("Edge assignments:", flow.flow)
+
+# Output:
+# Max flow: 6.0
+# Edge assignments [1.0, 5.0, 1.0, 2.0, 3.0, 3.0, 3.0]
+
+fig, ax = plt.subplots()
+ig.plot(
+    g,
+    target=ax,
+    layout="circle",
+    vertex_label=range(g.vcount()),
+    vertex_color="lightblue"
+)
+plt.show()
